f3d9ba360b31764bfafe3b6e2670607b99dcad07
Renan Germano <61218420+renangfs@users.noreply.github.com>
2021-07-31 11:39:36 -0300

    Add files via upload

---
+++ ProAgendaV1/conn.php
@@ -0,0 +1,10 @@
+<?php
+
+        $servidor = "localhost";
+        $usuario = "id14771691_use";
+        $senha = "Renan!@#123456";
+        $dbname = "id14771691_name";
+
+        //Cria conexão
+        $conn = mysqli_connect($servidor,$usuario,$senha,$dbname);
+        ?>
